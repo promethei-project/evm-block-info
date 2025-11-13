2c88455069b52ea1d778e3fef786966e0c193d52
Mark Spanbroek <mark@spanbroek.net>
2025-11-13 11:00:10 +0100

    print block numbers and hashes for archivist testnet

---
+++ scripts/testnet-info.js
@@ -0,0 +1,13 @@
+import { network } from "hardhat"
+
+const { ethers } = await network.connect("archivistTestnet")
+
+const address = "0x95658FdA29e3b547107c95c11dD5e4a1A034C4AB"
+const contract = await ethers.getContractAt("BlockInfo", address)
+
+while (true) {
+  let blockInfo = await contract.blockInfo()
+  console.log("block number: " + blockInfo[0])
+  console.log("block hashes: " + blockInfo[1].join())
+  await new Promise(r => setTimeout(r, 12000))
+}
